368b9af8d5255819dd9abf18b567b97b8411e829
Kristopher Herbert <klh55@pct.edu>
2025-04-03 17:26:12 -0400

    Apprentice Moves

--- Assets/Scripts/ApprenticeController.cs
+++ Assets/Scripts/ApprenticeController.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class ApprenticeController : MonoBehaviour
 {
     private SpriteRenderer rend;
+    private Animator animator;
+    
+    public int direction = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     
     void FixedUpdate()
     {
-       
+        animator.SetFloat("Speed", Mathf.Abs(direction));
+        RaycastHit2D appHit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+        Debug.DrawRay(transform.position, new Vector2(0, -2), Color.red, 0.5f);
+        Debug.Log(appHit);
+
+        if (appHit.collider == null)
+        {
+            direction = direction * -1;
+            rend.flipX = !rend.flipX;
+        }
+
+        transform.position = Vector2.Lerp(transform.position, new Vector2(transform.position.x - 1 * direction, transform.position.y), Time.deltaTime);
     }
 }
